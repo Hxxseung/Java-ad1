211363885b39d79366f75ba25458ddb2e76e97e2
Hxxseung <soaorsith2@naver.com>
2024-11-21 17:15:52 +0900

    concurrent.Lock End

---
+++ src/thread/sync/BankAccountV4.java
@@ -0,0 +1,47 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+        lock.lock(); // ReentrantLock 이용하여 lock을 걸기 , 객체 내부의 모니터 락이 아님, Lock 인터페이스와 ReetrantLock 이 제공하는 기능
+        try {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
+        } finally {
+            lock.unlock(); // ReentrantLock 이용하여 lock 해제, lock 을 반납하고 다른 스레드로 lock 을 넘겨줌
+        }
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock(); // ReentrantLock 이용하여 lock 걸기
+        try {
+            return balance;
+        } finally {
+            lock.unlock(); // ReentrantLock 이용하여 lock 해제
+        }
+    }
+}
